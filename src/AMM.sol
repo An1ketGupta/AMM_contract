384b6966d056519aebed44ddae159bd96c205b0b
Aniket Gupta <guptaaniket600.ag@gmail.com>
2025-12-31 17:21:10 +0000

    Completing the contract

--- src/AMM.sol
+++ src/AMM.sol
@@ -1,101 +1,191 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.13;
-import { Math } from "../lib/openzeppelin-contracts/contracts/utils/math/Math.sol";
-import { ERC20 } from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import { IERC20 } from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import { Ownable } from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+pragma solidity ^0.8.19;
+import {
+    Math
+} from "../lib/openzeppelin-contracts/contracts/utils/math/Math.sol";
 
-interface EthContract is IERC20{
-    function _requestAirdrop(uint _amount) external;
-}
-interface UsdcContract is IERC20{
-    function _requestAirdrop(uint _amount) external;
+import {
+    ERC20
+} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+import {
+    IERC20
+} from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+import {
+    ReentrancyGuard
+} from "../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+
+interface EthContract is IERC20 {
 }
 
-contract AMMContract is ERC20{
-    uint public reserveEth;
-    uint public reserveUSDC;
-    address constant DEAD = address(0xdead);
+interface UsdcContract is IERC20 {
+}
+
+contract AMMContract is ERC20, ReentrancyGuard {
+    uint private reserveEth;
+    uint private reserveUSDC;
+
+    uint constant private MINIMUM_LIQUIDITY = 1000;
     EthContract public ethToken;
     UsdcContract public usdcToken;
 
-    constructor(address _tokenA , address _tokenB) ERC20("mAniketCoin" , "mANC"){
-        ethToken = EthContract(_tokenA);
-        usdcToken = UsdcContract(_tokenB);
+    constructor(
+        address ethAddress,
+        address usdcAddress
+    ) ERC20("AniketCoin", "ANC") {
+        require(ethAddress != address(0), "Invalid Eth address");
+        require(usdcAddress != address(0), "Invalid USDC address");
+        ethToken = EthContract(ethAddress);
+        usdcToken = UsdcContract(usdcAddress);
+        reserveEth = 0;
+        reserveUSDC = 0;
     }
 
-    function min(uint num1 , uint num2) internal pure returns (uint){
-        if(num1 < num2) return num1;
-        else{
-            return num2;
+    function getActualTokenAmount(
+        uint ethAmount,
+        uint usdcAmount
+    ) internal view returns (uint, uint) {
+        if (reserveEth == 0 || reserveUSDC == 0) {
+            return (ethAmount, usdcAmount);
+        } else {
+            uint usdcForEth = (reserveUSDC * ethAmount) / reserveEth;
+            uint ethForUsdc = (reserveEth * usdcAmount) / reserveUSDC;
+            if (usdcForEth <= usdcAmount) {
+                return (ethAmount, usdcForEth);
+            } else {
+                return (ethForUsdc, usdcAmount);
+            }
         }
     }
 
-    function Stake(uint _amountA , uint _amountB) public{
-        require(_amountA > 0 , "Can't stake zero tokens");
-        require(_amountB > 0 , "Can't stake zero tokens");
+    function calculateShare(
+        uint eth_amount,
+        uint usdc_amount
+    ) public view returns (uint) {
+        
+        if (totalSupply() == 0) {
+            
+            uint shares = Math.sqrt(eth_amount * usdc_amount);
+            require(shares > MINIMUM_LIQUIDITY, "Insuffient tokens");
+            return shares-MINIMUM_LIQUIDITY;
 
-        if(_amountA > 0){
-            bool success = ethToken.transferFrom(msg.sender, address(this), _amountA);
-            require(success , "Didn't recieve eth from your wallet");
+        } else {
+
+            uint shares = Math.min(
+                (eth_amount * totalSupply()) / reserveEth,
+                (usdc_amount * totalSupply()) / reserveUSDC
+            );
+            return shares;
+
         }
-        if(_amountB > 0){
-            bool success = usdcToken.transferFrom(msg.sender, address(this), _amountB);
-            require(success , "Didn't recieve USDC from your wallet");
-        }
-        // taking a platform fee of 0.3 percent
-        uint shares = 0;
+    }
+
+    function _sync() internal{
+        reserveEth = ethToken.balanceOf(address(this));
+        reserveUSDC = usdcToken.balanceOf(address(this));
+    }
+    
+    function stake(
+        uint amount_Eth,
+        uint amount_USDC,
+        uint minshares
+    ) public nonReentrant {
+
+        require(amount_Eth > 0 && amount_USDC > 0, "Cannot stake zero tokens");
+        uint neededETH;
+        uint neededUSDC;
+        
+        uint balanceBefore = ethToken.balanceOf(address(this));
+        bool successEth = ethToken.transferFrom(msg.sender, address(this), amount_Eth);
+        require(successEth, "Didnt receive Eth tokens");
+        uint receivedEth = ethToken.balanceOf(address(this)) - balanceBefore;
+
+        balanceBefore = usdcToken.balanceOf(address(this));
+        bool successUsdc = usdcToken.transferFrom(msg.sender, address(this), amount_USDC);
+        require(successUsdc, "Didnt receive USDC tokens");
+        uint receivedUsdc = usdcToken.balanceOf(address(this)) - balanceBefore;
+
+        (neededETH, neededUSDC) = getActualTokenAmount(receivedEth, receivedUsdc);
+        uint shares = calculateShare(neededETH, neededUSDC); 
+
+        require(shares >= minshares, "Minimum slippage violation.");
+        
         if(totalSupply() == 0){
-            shares = Math.sqrt(_amountA * _amountB) - 1000;
-            require(shares > 0 , "Initial deposit too small");
-            _mint(DEAD, 1000);
+            _mint(address(1), MINIMUM_LIQUIDITY);
         }
-        else{
-            shares = min((_amountA * totalSupply())/reserveEth , (_amountB * totalSupply())/reserveUSDC);
-            require(shares > 0 , "Initial deposit too small");
+        
+        if (receivedEth > neededETH) {
+            ethToken.transfer(msg.sender, receivedEth - neededETH);
         }
+        if (receivedUsdc > neededUSDC) {
+            usdcToken.transfer(msg.sender, receivedUsdc - neededUSDC);
+        }
+        
         _mint(msg.sender, shares);
-        reserveEth += _amountA;
-        reserveUSDC += _amountB;
+        _sync();
     }
 
-    function SwapEthtoUSDC(uint _amount) public{
-        require(_amount > 0 , "Can't swap zero tokens");
-        
-        bool success = ethToken.transferFrom(msg.sender, address(this), _amount);
-        require(success , "Transaction blocked.");
+    function swapEthtoUsdc(
+        uint ethAmount,
+        uint minUSDC
+        ) public nonReentrant{
+            
+        require(ethAmount > 0 , "Can't swap 0 tokens");
+        require(reserveEth > 0 && reserveUSDC > 0 , "No liquidity in the pool");
+        uint ethBefore = ethToken.balanceOf(address(this));
+        bool success = ethToken.transferFrom(msg.sender, address(this), ethAmount);
+        require(success , "Didn't receive eth tokens");
+        uint receivedEth = ethToken.balanceOf(address(this)) - ethBefore;
 
-        uint netAmount = (_amount*997)/1000;
+        uint netEth = (receivedEth * 997)/1000;
+        uint USDCtokens = (reserveUSDC * netEth)/(reserveEth + netEth);
+        require(USDCtokens >= minUSDC , "Minimum slippage violation.");
 
-        uint returnUSDC = (reserveUSDC * netAmount)/(reserveEth + _amount);
-        success = usdcToken.transfer(msg.sender, returnUSDC);
-        reserveEth += _amount;
-        reserveUSDC -= returnUSDC;
+        usdcToken.transfer(msg.sender, USDCtokens);
+        _sync();
+    }
+    
+    function swapUsdcToEth(
+        uint usdcAmount,
+        uint minEth
+        ) public nonReentrant{
+            
+        require(usdcAmount > 0 , "Can't swap 0 tokens");
+        require(reserveEth > 0 && reserveUSDC > 0 , "No liquidity in the pool");
+        uint usdcBefore = usdcToken.balanceOf(address(this));
+        bool success = usdcToken.transferFrom(msg.sender, address(this), usdcAmount);
+        require(success , "Didn't receive eth tokens");
+        uint receivedUsdc = usdcToken.balanceOf(address(this)) - usdcBefore;
+
+        uint netUsdc = (receivedUsdc * 997)/1000;
+        uint ETHTokens = (reserveEth * netUsdc)/(reserveUSDC + netUsdc);
+        require(ETHTokens >= minEth , "Minimum slippage violation.");
+
+        ethToken.transfer(msg.sender, ETHTokens);
+        _sync();
     }
 
-    function SwapUSDCtoEth(uint _amount) public{
-        require(_amount > 0 , "Can't swap zero tokens");
-        
-        bool success = usdcToken.transferFrom(msg.sender, address(this), _amount);
-        require(success , "Transaction blocked.");
+    function removeLiquidity(
+    uint shares
+    ) public nonReentrant {
+        require(shares > 0 , "No shares provided");
 
-        uint netAmount = (_amount*997)/1000;
+        _sync();
 
-        uint returnEth = (reserveEth * netAmount)/(reserveUSDC + _amount);
-        success = ethToken.transfer(msg.sender, returnEth);
-        reserveEth -= returnEth;
-        reserveUSDC += _amount;
-    }
+        require(reserveEth > 0 && reserveUSDC > 0, "Pool is empty");
+        uint ethAmount = (shares * reserveEth) / totalSupply();
+        uint usdcAmount = (shares * reserveUSDC) / totalSupply();
+        require(totalSupply() - shares >= MINIMUM_LIQUIDITY,"Cannot remove minimum liquidity");
+        _burn(msg.sender, shares);
 
-    function Withdraw(uint _amount) public{
-        require(_amount > 0 , "Amount entered is 0");
-        uint returnEth = (_amount * reserveEth)/totalSupply();
-        uint returnUSDC = (_amount * reserveUSDC)/totalSupply();
+        reserveEth = reserveEth - ethAmount;
+        reserveUSDC = reserveUSDC - usdcAmount;
 
-        _burn(msg.sender, _amount);
-        ethToken.transfer(msg.sender, returnEth);
-        usdcToken.transfer(msg.sender, returnUSDC);
-        reserveEth -= returnEth;
-        reserveUSDC -= returnUSDC;
+        bool success1 = ethToken.transfer(msg.sender, ethAmount);
+        bool success2 = usdcToken.transfer(msg.sender, usdcAmount);
+
+        require(success1, "ETH transfer failed");
+        require(success2, "USDC transfer failed");
     }
 }
